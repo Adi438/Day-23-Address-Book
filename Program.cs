7594c964eed85dfe73448e61f10ed98c1b349eb6
Adi438 <mishraaaditya54@gmail.com>
2022-06-05 01:27:52 +0530

    Adding code

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace addressbook2
+namespace AddressBookUsingCollectionLibrary
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to AddressBook problem");
+
+            Records records = new Records();
+
+
+            //Infinite Loop
+            while (true)
+            {
+                Console.WriteLine("\nEnter 1 to Add person in AddressBook");
+                Console.WriteLine("Enter 2 to Display");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        records.AddRecord();
+                        break;
+                    case 2:
+                        records.ToPrint();
+                        break;
+                    default:
+                        Console.WriteLine("Pleasee select valid input");
+                        break;
+                }
+
+            }
         }
     }
 }
+
